e2bd446a0dc7b2019e72a61509ef06bcdab6c616
Anuj Kumar <anuj39263@gmail.com>
2026-04-17 23:11:45 +0530

    Added rate limiter to imp routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 import {signUp, login, authenticate} from "../controllers/user.js";
 import {forgetPassword, resetPassword, updatePassword} from "../controllers/forgetPassord.js"
+import {authLimiter, thirdPartyStrictLimiter} from "../middleware/rateLimitter.js"
 import {googleSignup, googleLogin} from "../controllers/googleAuth.js"
 import auth from "../middleware/auth.js"
 import express from 'express';
@@ -8,11 +9,11 @@ const router=express.Router()
 
 router.post("/google-signup", googleSignup)
 router.post("/google-login", googleLogin)
-router.post("/login", login)
-router.post("/forgetpassword", auth, forgetPassword)
+router.post("/login", authLimiter, login)
+router.post("/forgetpassword", thirdPartyStrictLimiter, auth, forgetPassword)
 router.get("/resetpassword/:id",  resetPassword)
 router.post("/updatepassword/:resetpasswordid", updatePassword)
-router.post("/signup", signUp)
+router.post("/signup", authLimiter, signUp)
 router.get("/authenticate", auth, authenticate)
 
 export default router;
