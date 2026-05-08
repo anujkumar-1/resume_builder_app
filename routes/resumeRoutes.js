00ff2edbd6c34a38728d3a88a1ccd07c35389619
Anuj Kumar <anuj39263@gmail.com>
2026-05-08 23:13:05 +0530

    Adding Permission Middleware to routes

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,26 +1,26 @@
 import {updateContactInfo, updateExperience, updateSkills, updateEducation, updateSummary, updateLanguages, updateCertificates, updateAwards, getResumeInfo,  updateEditExperience, updateEditEducation, downloadResume, updateIntrest, updateProjects, deleteEduItem, deleteExpItem, deleteProjectItem} from "../controllers/resume.js"
 import auth from "../middleware/auth.js"
 import {thirdPartyStrictLimiter} from "../middleware/rateLimitter.js"
-
+import {checkPermission} from "../middleware/access.js"
 import express from 'express';
 const router=express.Router()
 
-router.post("/updateContactInfo", auth, updateContactInfo)
-router.post("/updateExperience", auth, updateExperience)
-router.post("/updateSkills", auth, updateSkills)
-router.post("/updateEducation", auth, updateEducation)
-router.post("/updateSummary", auth, updateSummary)
-router.post("/updateLanguages", auth, updateLanguages)
-router.post("/updateCertificates", auth, updateCertificates)
-router.post("/updateAwards", auth, updateAwards)
-router.post("/updateIntrest", auth, updateIntrest)
-router.post("/updateProject", auth, updateProjects)
+router.post("/updateContactInfo", auth, checkPermission("EDIT_RESUME"), updateContactInfo)
+router.post("/updateExperience", auth, checkPermission("EDIT_RESUME"), updateExperience)
+router.post("/updateSkills", auth, checkPermission("EDIT_RESUME"), updateSkills)
+router.post("/updateEducation", auth, checkPermission("EDIT_RESUME"), updateEducation)
+router.post("/updateSummary", auth, checkPermission("EDIT_RESUME"), updateSummary)
+router.post("/updateLanguages", auth, checkPermission("EDIT_RESUME"), updateLanguages)
+router.post("/updateCertificates", auth, checkPermission("EDIT_RESUME"), updateCertificates)
+router.post("/updateAwards", auth, checkPermission("EDIT_RESUME"), updateAwards)
+router.post("/updateIntrest", auth, checkPermission("EDIT_RESUME"), updateIntrest)
+router.post("/updateProject", auth, checkPermission("EDIT_RESUME"), updateProjects)
 router.get("/getResumeInfo", auth, getResumeInfo)
-router.post("/updateEditExperience", auth, updateEditExperience)
-router.post("/updateEditEducation", auth, updateEditEducation)
-router.get("/downloadResume", thirdPartyStrictLimiter, auth, downloadResume)
-router.delete("/deleteEducationItem", auth, deleteEduItem)
-router.delete("/deleteExperienceItem", auth, deleteExpItem)
-router.delete("/deleteProjectItem", auth, deleteProjectItem)
+router.post("/updateEditExperience", auth, checkPermission("EDIT_RESUME"), updateEditExperience)
+router.post("/updateEditEducation", auth, checkPermission("EDIT_RESUME"), updateEditEducation)
+router.get("/downloadResume", thirdPartyStrictLimiter, auth, checkPermission("DOWNLOAD_RESUME"), downloadResume)
+router.delete("/deleteEducationItem", auth, checkPermission("DELETE_RESUME_FIELD"), deleteEduItem)
+router.delete("/deleteExperienceItem", auth, checkPermission("DELETE_RESUME_FIELD"), deleteExpItem)
+router.delete("/deleteProjectItem", auth, checkPermission("DELETE_RESUME_FIELD"), deleteProjectItem)
 
 export default router;
