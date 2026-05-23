41f051ac6ad32ef761adf57238fb23dda6da158f
Anuj Kumar <anuj39263@gmail.com>
2026-05-23 22:54:40 +0530

    Updated Auth Middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,29 @@
-import User from "../models/user.js"
-import jwt from "jsonwebtoken"
+import {User} from "../models/index.js";
+import jwt from "jsonwebtoken";
 
-const auth = async (req, res, next)=>{
-    try {
-        const Authtoken = req.header("Authorization")
-        if(Authtoken){
-            const token = Authtoken.split(' ')[1];
-            const user= jwt.verify(Authtoken, process.env.JWT_TOKEN_SECRET, (err, user)=>{
-                if(err){
-                    return res.sendStatus(403); // Invalid token
-                }
-                req.user = user;
-                console.log("user", user)
-                console.log("req.user", req.user)
-                next();
-            }) 
-        }
+const auth = async (req, res, next) => {
+  try {
+
+        const AuthToken = req.headers['authorization']; 
         
-    } catch (error) {
-        console.error("auth :", error)
-        res.status(500).json({message: "missing credentials, please login again"})
-    }
-}
+        if(!AuthToken){
+            return res.status(401).json({ message: "No token provided" });
+        }
+        const decoded = jwt.verify(AuthToken, process.env.JWT_TOKEN_SECRET);
+        let id = decoded.userId
+        const user = await User.findById(id);
+
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+      req.user = user;
+
+      next();
+    
+  } catch (error) {
+    console.error("auth error:", error);
+    throw new Error("Auth Error")
+  }
+};
+
 export default auth;
